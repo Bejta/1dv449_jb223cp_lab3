47a614cb119cc0a81220ef5e9e6c07793d83b87f
Jasmin Bejtovic <jb223cp@student.lnu.se>
2015-12-15 20:44:05 +0100

    Change my mind and added Google Maps instead

--- Mashup/App_Start/BundleConfig.cs
+++ Mashup/App_Start/BundleConfig.cs
@@ -19,6 +19,10 @@ namespace Mashup
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/Maps.js"));
+
+            
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
---
+++ Mashup/Scripts/Maps.js
@@ -0,0 +1,16 @@
+﻿//function GetMap() {
+//    var mapOptions = {
+//        credentials: "Ar7yrfT4Hux1-O7311-MEO-utjJJKIpiZbUwuZsxA3KhUU-UT5Oisz1kmxE9EnOQ",
+//    }
+//    var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+//    var viewBoundaries = Microsoft.Maps.LocationRect.fromLocations(new Microsoft.Maps.Location(62.02, 15.38), new Microsoft.Maps.Location(47.620700, -122.347584), new Microsoft.Maps.Location(47.622052, -122.345869));
+
+//    map.setView({ bounds: viewBoundaries });
+//    //map.focus.
+//}
+
+<!-- This code tells the browser to execute the "Initialize" method 
+only when the complete document model has been loaded. -->
+$(document).ready(function () {
+    Initialize(); 
+});  
